ea3b1a42817bbe2ed7369f931c133c4c6f2ef8a3
Zach Latta <zach@zachlatta.com>
2016-04-06 19:13:38 -0700

    Add click listeners to buttons

--- workshops/soundboard/examples/final/main.js
+++ workshops/soundboard/examples/final/main.js
@@ -1,3 +1,15 @@
 var cat = $("#cat");
 var frog = $("#frog");
 var cow = $("#cow");
+
+cat.click(function () {
+  console.log("Meow");
+});
+
+frog.click(function () {
+  console.log("[frog sound]");
+});
+
+cow.click(function () {
+  console.log("Moo");
+});

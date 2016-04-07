938215e0dc24e2ae8e2d44899ee535e740316a55
Zach Latta <zach@zachlatta.com>
2016-04-06 19:28:45 -0700

    Actually play sounds on click
    Note: a single sound cannot be played multiple times at once because
    there's only one Audio object per sound. If you want to play a single
    sound multiple times at once, you need to create an Audio object in the
    callback itself.

--- workshops/soundboard/examples/final/main.js
+++ workshops/soundboard/examples/final/main.js
@@ -2,14 +2,18 @@ var cat = $("#cat");
 var frog = $("#frog");
 var cow = $("#cow");
 
+var catSound = new Audio("sounds/cat.mp3");
+var frogSound = new Audio("sounds/frog.mp3");
+var cowSound = new Audio("sounds/cow.mp3");
+
 cat.click(function () {
-  console.log("Meow");
+  catSound.play();
 });
 
 frog.click(function () {
-  console.log("[frog sound]");
+  frogSound.play();
 });
 
 cow.click(function () {
-  console.log("Moo");
+  cowSound.play();
 });
